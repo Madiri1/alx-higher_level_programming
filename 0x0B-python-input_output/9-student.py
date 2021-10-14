8d2dd11e3aa737607f1fd2c997b9426716de3f5a
Pascal Madiri <psclmadiri9@gmail.com>
2021-10-14 02:04:29 +0000

    class Student that defines a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 # 9-student.py
-"""Defines a class student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """represent a student"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """initialise a student
-        
+        """Initialize a new Student.
         Args:
-            first_name (str): The first name of the student
-            last_name (str): The last name of the student
-            age (int): The age of the student
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_jason_file(self):
-        """Get a dictionary representation of the student"""
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
         return self.__dict__
